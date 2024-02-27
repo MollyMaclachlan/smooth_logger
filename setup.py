6c54d8f16fb50515496f06685cc097c0ddf8d4f6
Murdo B. Maclachlan <murdomaclachlan@duck.com>
2024-02-27 18:55:05 +0000

    Release 0.2.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name="smooth_logger",
-    version="0.2.0",
+    version="0.2.1",
     author="Murdo Maclachlan",
     author_email="murdomaclachlan@duck.com",
     description=(
