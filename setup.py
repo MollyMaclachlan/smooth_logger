3d4a8ccf88224f77cff350491d3a03a036c94011
Murdo B. Maclachlan <murdomaclachlan@duck.com>
2024-05-08 11:18:02 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name="smooth_logger",
-    version="1.0.1",
+    version="1.0.2",
     author="Murdo Maclachlan",
     author_email="murdomaclachlan@duck.com",
     description=(
