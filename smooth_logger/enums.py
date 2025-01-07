109bb7df05590092004c37c70724716e6351f5f4
Murdo B. Maclachlan <murdomaclachlan@duck.com>
2025-01-07 16:30:54 +0000

    File formatting

--- smooth_logger/enums.py
+++ smooth_logger/enums.py
@@ -5,5 +5,4 @@ class Categories(Enum):
     PRINT    = 1  # print to console but do not save to log file
     SAVE     = 2  # save to log file but do not print to console
     MAXIMUM  = 3  # print to console and save to log file
-
     ENABLED  = 4  # DEPRECATED: print to console but do not save to log file
